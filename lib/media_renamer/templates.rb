48356567edff7e5f7a9f6f85462a589e25ac6b0c
John Tajima <manjiro@gmail.com>
2017-08-12 20:23:16 -0400

    better template handling and update template

--- lib/media_renamer/templates.rb
+++ lib/media_renamer/templates.rb
@@ -4,11 +4,11 @@ module MediaRenamer
     extend self
 
     def movie_template
-      @movie_template ||= Liquid::Template.parse(SETTINGS['MOVIE_TEMPLATE'])
+      @movie_template ||= Liquid::Template.parse(SETTINGS['MOVIE_TEMPLATE'], error_mode: :strict)
     end
 
     def tv_template
-      @tv_template ||= Liquid::Template.parse(SETTINGS['TV_TEMPLATE'])
+      @tv_template ||= Liquid::Template.parse(SETTINGS['TV_TEMPLATE'],error_mode: :strict)
     end
 
     def render_movie(movie, mediafile, options = {})
